a19174e993cc638dc35aa9e9d62f91672e337594
Vodoprovod <naiznanku45@mail.ru>
2019-03-20 00:31:56 +0300

    devtools installed

--- react-practice-master/src/store.js
+++ react-practice-master/src/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
+import { DevTools } from './utils/index'
 
 
 function _applyMiddleware() {
@@ -13,7 +14,7 @@ function _applyMiddleware() {
 
 export default function configureStore(initialState) {
 
-    const store = compose(_applyMiddleware())(createStore)(rootReducer, initialState);
+    const store = compose(_applyMiddleware(), DevTools.instrument())(createStore)(rootReducer, initialState);
 
     return store;
     
